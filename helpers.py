000dcbc5fbcab5be7c3a30a0c488de3d79dbb9eb
mustious <mustious@gmail.com>
2023-04-30 20:56:46 +0100

    add display_image_prediction functions

--- helpers.py
+++ helpers.py
@@ -24,3 +24,29 @@ def display_images(image_dataloader: DataLoader, class_labels, n_rows: int =3, n
         axs[i%n_rows, i//n_rows].imshow(image.numpy(), cmap="gray")
         axs[i%n_rows, i//n_rows].set(xticks=[], yticks=[])
         axs[i%n_rows, i//n_rows].set_title(class_labels[label[i]])
+
+
+def display_predicted_images(model, image_dataloader: DataLoader, class_labels, n_rows: int =3, n_cols:int =5, figsize: tuple =(12,6)):
+
+    """
+    displays images from dataloader
+    """
+    # display MNIST sample images
+    n_samples = n_rows * n_cols # number sample images to display
+
+    data, label = next(iter(test_dataloader))
+    model = model.to("cpu")
+
+    model_predictions_logits = model(data)
+    predictions = nn.functional.softmax(model_predictions_logits, dim=0).argmax(dim=1)
+
+
+    fig, axs = plt.subplots(nrows=n_rows, ncols=n_cols, figsize=figsize)
+
+    for i, image in enumerate(data[:n_samples]):
+        image = image.detach()[0]
+        #image = image.squeeze(dim=0)
+        axs[i%n_rows, i//n_rows].imshow(image.numpy(), cmap="gray")
+        axs[i%n_rows, i//n_rows].set(xticks=[], yticks=[])
+        title = f"Actutual Class {class_labels[label[i]]}\n Predicted Class: {class_labels[predictions[i]]}"
+        axs[i%n_rows, i//n_rows].set_title(title)
